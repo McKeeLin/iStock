582617316dcf7f52f9db310da2765461d076a7f4
McKeeLin <mckeelin@icloud.com>
2015-07-01 17:35:20 +0800

    no message

---
+++ iStock/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  iStock
+//
+//  Created by game-netease on 15/6/4.
+//  Copyright (c) 2015年 McKeeLin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+@property NSString *code;
+
+
+@end
+
