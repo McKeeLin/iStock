5a6b4c83330162fdeace3a503e7d43764bab87cb
McKee <McKee@NetEase-McKee.local>
2016-08-03 10:18:56 +0800

    ADD ICONS

---
+++ iStock/Data/mySQL.h
@@ -0,0 +1,13 @@
+//
+//  mySQL.h
+//  iStock
+//
+//  Created by McKee on 15/8/20.
+//  Copyright (c) 2015年 McKeeLin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mySQL : NSObject
+
+@end
---
+++ iStock/Data/mySQL.m
@@ -0,0 +1,13 @@
+//
+//  mySQL.m
+//  iStock
+//
+//  Created by McKee on 15/8/20.
+//  Copyright (c) 2015年 McKeeLin. All rights reserved.
+//
+
+#import "mySQL.h"
+
+@implementation mySQL
+
+@end
